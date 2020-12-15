3f5e0db4e154dcb683182640215d11c7cd45a572
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-12-15 11:31:24 +0400

    create packages

--- app/src/main/java/com/example/filmsapp/activities/MainActivity.java
+++ app/src/main/java/com/example/filmsapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.filmsapp;
+package com.example.filmsapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.filmsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
