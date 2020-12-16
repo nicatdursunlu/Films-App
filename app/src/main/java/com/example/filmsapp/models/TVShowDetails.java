0bb6aac5270be5061e029b05275a1aa8cc67f9e1
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-12-16 18:25:34 +0400

    add TVShowsDEtails and Episode

---
+++ app/src/main/java/com/example/filmsapp/models/TVShowDetails.java
@@ -0,0 +1,58 @@
+package com.example.filmsapp.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TVShowDetails {
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("runtime")
+    private String runtime;
+
+    @SerializedName("image_path")
+    private String imagePath;
+
+    @SerializedName("rating")
+    private String rating;
+
+    @SerializedName("genres")
+    private String[] genres;
+
+    @SerializedName("episodes")
+    private List<Episode> episodes;
+
+    
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String[] getGenres() {
+        return genres;
+    }
+
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+}
