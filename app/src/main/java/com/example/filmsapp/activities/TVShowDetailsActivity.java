516691c644eb39589b17a42a15d2381cc665ea59
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-12-18 10:51:13 +0400

    add back button

--- app/src/main/java/com/example/filmsapp/activities/TVShowDetailsActivity.java
+++ app/src/main/java/com/example/filmsapp/activities/TVShowDetailsActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.example.filmsapp.R;
 import com.example.filmsapp.adapters.ImageSliderAdapter;
@@ -31,6 +31,7 @@ public class TVShowDetailsActivity extends AppCompatActivity {
 
     private void doInitialization() {
         tvShowDetailsViewModel = new ViewModelProvider(this).get(TVShowDetailsViewModel.class);
+        activityTVShowDetailsBinding.imageBack.setOnClickListener(v -> onBackPressed());
         getTVShowDetails();
     }
 
@@ -44,6 +45,10 @@ public class TVShowDetailsActivity extends AppCompatActivity {
                         if(tvShowDetailsResponse.getTvShowDetails().getPictures() != null) {
                             loadImageSlider(tvShowDetailsResponse.getTvShowDetails().getPictures());
                         }
+                        activityTVShowDetailsBinding.setTvShowImageURL(
+                                tvShowDetailsResponse.getTvShowDetails().getImagePath()
+                        );
+                        activityTVShowDetailsBinding.imageTvShow.setVisibility(View.VISIBLE);
                     }
                 }
         );
@@ -55,6 +60,13 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         activityTVShowDetailsBinding.sliderViewPager.setVisibility(View.VISIBLE);
         activityTVShowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
         setupSliderIndicators(sliderImages.length);
+        activityTVShowDetailsBinding.sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentSliderIndicator(position);
+            }
+        });
     }
 
     private void setupSliderIndicators(int count) {
@@ -73,6 +85,23 @@ public class TVShowDetailsActivity extends AppCompatActivity {
             activityTVShowDetailsBinding.layoutSliderIndicators.addView(indicators[i]);
         }
         activityTVShowDetailsBinding.layoutSliderIndicators.setVisibility(View.VISIBLE);
+        setCurrentSliderIndicator(0);
+    }
+
+    private void setCurrentSliderIndicator(int position) {
+        int childCount = activityTVShowDetailsBinding.layoutSliderIndicators.getChildCount();
+        for(int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) activityTVShowDetailsBinding.layoutSliderIndicators.getChildAt(i);
+            if(i == position) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_slider_indicator_active)
+                );
+            } else  {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_sllider_image_inactive)
+                );
+            }
+        }
     }
 
 }
